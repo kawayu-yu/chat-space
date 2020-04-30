9b730bf594b9f353408f1595a48b5ae4483f1bdc
kawayu-yu <yuya.2a.0758@gmail.com>
2020-04-30 18:18:28 +0900

    create message model

---
+++ db/migrate/20200430090342_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.text    :body
+      t.text    :image
+      t.integer :user,  foreign_key: true
+      t.integer :group, foreign_key: true
+      t.timestamps
+    end
+  end
+end
