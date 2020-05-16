e8d85a0ad5bee78544c206670d39ff166a380105
kawayu-yu <yuya.2a.0758@gmail.com>
2020-05-16 15:14:27 +0900

    test

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe Message do
+  describe '#create' do
+    context 'messageを保存できる場合' do
+      it "メッセージがあれば保存できる" do
+        expect(build(:message, image: nil)).to be_valid
+      end
+
+      it "画像があれば保存できる" do
+        expect(build(:message, body: nil)).to be_valid
+      end
+
+      it "メッセージと画像があれば保存できる" do
+        expect(build(:message)).to be_valid
+      end
+    end
+
+
+
+    context 'messageを保存できない場合' do
+
+      it "メッセージも画像も無いと保存できない" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        expect(message.errors[:body]).to include("を入力してください")
+      end
+
+      it 'group_idが無いと保存できないこと' do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include("を入力してください")
+      end
+
+      it ' user_idが無いと保存できないこと' do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include("を入力してください")
+      end
+    end
+  end
+end
