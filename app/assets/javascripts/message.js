9f82107ead26c3c1fa70fce023dce3035309676e
kawayu-yu <yuya.2a.0758@gmail.com>
2020-05-19 15:45:39 +0900

    Implement_respond_to

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,13 +3,15 @@ $(function() {
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
+
+    console.log(url);
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
-      contentTYpe: false
+      contentType: false
     })
   })
 })
